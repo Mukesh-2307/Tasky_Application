be8df755ba7a5eb98af313ea7314f30e915bd4e3
Mukesh Kumar Dalai <99747839+Mukesh-2307@users.noreply.github.com>
2024-07-31 12:49:21 +0000

    reduced css by using bootstrap and some minor edits

---
+++ script.js
@@ -0,0 +1,19 @@
+// let state = {
+//     taskList: [
+//         {
+//             imageURL:"",
+//             taskTitle:"",
+//             taskDesc:""
+//         }
+//     ]
+// }
+
+const state = {
+    taskList: []
+}
+
+const taskContents = document.querySelector(".task__contents");
+const taskModal = document.querySelector(".modal-body");
+
+console.log(taskContents);
+console.log(taskModal);
