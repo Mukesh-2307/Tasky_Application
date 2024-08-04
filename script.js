b4c367ccd37e458729d3ef1d67db5dc10cf960cb
Mukesh Kumar Dalai <99747839+Mukesh-2307@users.noreply.github.com>
2024-08-04 03:18:49 +0000

    done with handling submit button

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// example :
 // let state = {
 //     taskList: [
 //         {
@@ -8,6 +9,7 @@
 //     ]
 // }
 
+// similar to a backup storage but not exactly backup storage
 const state = {
   taskList: [],
 };
@@ -17,15 +19,13 @@ const taskModal = document.querySelector(".modal-body");
 
 var todays_date = new Date();
 console.log(todays_date.toDateString());
-// console.log(taskContents);
-// console.log(taskModal);
 
 // populating the cards container
 function htmlTaskContent({ id, url, title, desc }) {
   return `
     <div class="col-md-6 col-lg-4 mt-3" id=${id} key=${id}>
         <div class="card shadow-sm rounded task_card">
-            <div class-"card-header d-flex justify-content-end task_card_header">
+            <div class="card-header d-flex justify-content-end gap-2 task_card_header">
                 <button type="button" class="btn btn-outline-primary" name=${id}>
                     <i class="fa-regular fa-pen-to-square"></i>
                 </button>
@@ -41,7 +41,7 @@ function htmlTaskContent({ id, url, title, desc }) {
                 <h4 class="card-title task_card_title">${title}</h4>
                 <span class="card-text task_card_desc trim-3-lines text-muted">${desc}</span>
                 <div class="tags text-white d-flex flex-warp">
-                    <span class="badge bg-primary m-1">created on ${todays_date.toDateString()}</span>
+                    <span class="badge bg-primary">created on ${todays_date.toDateString()}</span>
                 </div>
             </div>
             <div class="card-footer ">
@@ -68,8 +68,42 @@ const htmlModalContent = ({ id, url, title, desc }) => {
     `;
 };
 
+// converting json file into a string
 const updateLocalStorage = () => {
-  localStorage.setItem("tasky", JSON.stringify({ tasks: state.taskList }));
+  console.log("updating localStorage");
+  localStorage.setItem("task", JSON.stringify({ tasks: state.taskList }));
 };
 
 // loading initial data
+
+// converting string into a json file
+const loadInitialData = () => {
+  const localStorageCopy = JSON.parse(localStorage.task);
+  console.log("got info from localStorage");
+  if (localStorageCopy) {
+    state.taskList = localStorageCopy.tasks;
+  }
+  console.log("updating state task list");
+  state.taskList.map((cardDate) => {
+    taskContents.insertAdjacentHTML("beforeend", htmlTaskContent(cardDate));
+  });
+};
+
+const handleSubmit = (e) => {
+  const cardId = `${Date.now()}`;
+  const input = {
+    url: document.getElementById("imageUrl").value,
+    title: document.getElementById("taskTitle").value,
+    desc: document.getElementById("taskDesc").value,
+  };
+  taskContents.insertAdjacentHTML(
+    "beforeend",
+    htmlTaskContent({ ...input, cardId })
+  );
+  console.log("done populating card section");
+
+  console.log("updating state task list with new values.");
+  state.taskList.push({ ...input, cardId });
+  console.log("updating localStorage with new values.");
+  updateLocalStorage();
+};
